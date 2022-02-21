e8ff7fd79d0a09d75fcaa2c317107963320e4904
kuolem96 <a.melouk@esi-sba.dz>
2022-02-21 03:40:39 +0100

    created page for schedule

---
+++ frontend/scripts/schedule.js
@@ -0,0 +1,16 @@
+function populateList(data, id) {
+  let list = document.getElementById(id)
+  list.replaceChildren()
+  let option = new Option('-- --', '')
+  option.disabled = true
+  option.selected = true
+  list.appendChild(option)
+  data.forEach(item => {
+    option = new Option(item.name, JSON.stringify(item))
+    list.appendChild(option)
+  })
+}
+
+let populate = () => {
+  getAllLinesNamesIdsDb().then(data => populateList(data, 'line_schedule'))
+}
